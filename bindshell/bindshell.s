901aacddfec38111906db3bbfba4e131ad5392fd
chrjoh <christerjoha@gmail.com>
2014-05-18 11:50:33 +0200

    Added license in all source files

--- bindshell/bindshell.s
+++ bindshell/bindshell.s
@@ -1,3 +1,18 @@
+; This file is part of shell_code.
+;
+;    shell_code is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 3 of the License.
+;
+;    shell_code is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with shell_code.  If not, see <http://www.gnu.org/licenses/>.
+
+
 BITS 64
 
 section .text
--- reverse_shell/reverse_shell_no_bad_values.s
+++ reverse_shell/reverse_shell_no_bad_values.s
@@ -1,3 +1,17 @@
+; This file is part of shell_code.
+;
+;    shell_code is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 3 of the License.
+;
+;    shell_code is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with shell_code.  If not, see <http://www.gnu.org/licenses/>.
+
 ; Example with bad values removed, not optimized for the size as we repeat operations that could be done once
 BITS 64
 
